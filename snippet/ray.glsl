004bb09048cab4e773c4ec0eafe1f52cc1736122
mastertth <tsukasa.japan1201@gmail.com>
2019-10-13 00:28:07 +0900

    add ray

---
+++ snippet/ray.glsl
@@ -0,0 +1,13 @@
+precision mediump float;
+/*
+uniform vec3 iResolution;   // viewport resolution (in pixels)
+uniform vec4 iMouse;        // mouse pixel coords. xy: current, zw: click
+uniform float iTime;        // shader playback time (in seconds)
+*/
+
+void main(void) 
+{
+    vec2 p = (gl_FragCoord.xy * 2.0 - iResolution.xy) / min(iResolution.x, iResolution.y);
+    float f = 0.01 / (abs(p.x) * abs(p.y));
+    gl_FragColor = vec4(vec3(f), 1.0);
+}
